a2133b199268aba0795cbf0fefe31f157744e71c
moizsabun <moizzoaib786@gmail.com>
2020-11-29 23:39:20 +0500

    added, css  and design components

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,23 @@
+import React , {useState}from 'react'
+
+
+export const AddTransaction = () => {
+    const [text, setText] =  useState('');
+    const [amount, setamount] =  useState('');
+    return (
+        <div>
+            <h3>Add New Transaction</h3>
+            <form id="form">
+                <div className="form-control">
+                    <label htmlFor="text"> Text</label>
+                    <input type="text" value={text} onChange={ (e) => {setText(e.target.value) }} placeholder= "Enter text..."></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount"> Amount <br/> (negative - expense , positive - income) </label>
+                    <input type="number" value={amount} onChange={ (e) => {setamount(e.target.value) }}  placeholder= "Enter amount..."></input>
+                </div>
+                <button className="btn"> Add Transaction</button>
+            </form>
+        </div>
+    )
+}
---
+++ src/components/TransactionList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const TransactionList = () => {
+    return (
+        <>
+            <h3>History</h3>
+            <ul  className="list">
+            <li className="minus">     
+            cash <span>-$400</span> <button className="delete-btn">x</button>
+            </li>
+            <li className="plus">     
+            cash <span>-$1100</span> <button className="delete-btn">x</button>
+            </li>
+            </ul>
+                
+
+        </>
+    )
+}
